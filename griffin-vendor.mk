5d1c922ae6a79256b32d5706fd240c66ae0d3639
Erfan Abdi <erfangplus@gmail.com>
2017-08-31 02:05:22 +0300

    griffin: fix rild target path
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- griffin-vendor.mk
+++ griffin-vendor.mk
@@ -1079,7 +1079,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/griffin/proprietary/vendor/lib/sensor_calibrate.so:system/vendor/lib/sensor_calibrate.so \
     vendor/motorola/griffin/proprietary/vendor/lib/sensors.ssc.so:system/vendor/lib/sensors.ssc.so \
     vendor/motorola/griffin/proprietary/vendor/qcril.db:system/vendor/qcril.db \
-    vendor/motorola/griffin/proprietary/bin/rild:vendor/bin/hw/rild \
+    vendor/motorola/griffin/proprietary/bin/rild:system/vendor/bin/hw/rild \
     vendor/motorola/griffin/proprietary/lib/libtinyalsa.so:system/lib/libtinyalsa.so \
     vendor/motorola/griffin/proprietary/lib64/libtinyalsa.so:system/lib64/libtinyalsa.so \
     vendor/motorola/griffin/proprietary/lib/hw/audio.primary.msm8996.so:system/lib/hw/audio.primary.msm8996.so
