96f5fbfaa247c2febdd65f0db74fa768cd070de4
Erfan Abdi <erfangplus@gmail.com>
2017-09-13 10:53:43 +0300

    griffin: HAX to load N libgui & ui for cam blobs
    sed -i "s/libgui.so/libHui.so/" libgui.so
    mv libgui.so libHui.so
    sed -i "s/libgui.so/libHui.so/" libmot_gpu_mapper.so
    # libHui.so is Nougat libgui.so
    
    sed -i "s/libui.so/libHu.so/" libui.so
    mv libui.so libHu.so
    sed -i "s/libui.so/libHu.so/" libmot_gpu_mapper.so
    sed -i "s/libui.so/libHu.so/" libHui.so
    # libHu.so is Nougat libui.so
    
    sed -i "s/libui.so/libHu.so/" camera.msm8996.so
    sed -i "s/libgui.so/libHui.so/" camera.msm8996.so
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- griffin-vendor.mk
+++ griffin-vendor.mk
@@ -1075,7 +1075,9 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/griffin/proprietary/vendor/lib/libprotobuf-cpp-shit.so:system/vendor/lib/libprotobuf-cpp-shit.so \
     vendor/motorola/griffin/proprietary/vendor/lib64/libprotobuf-cpp-shit.so:system/vendor/lib64/libprotobuf-cpp-shit.so \
     vendor/motorola/griffin/proprietary/lib/libmemalloc.so:system/lib/libmemalloc.so \
-    vendor/motorola/griffin/proprietary/lib64/libmemalloc.so:system/lib64/libmemalloc.so 
+    vendor/motorola/griffin/proprietary/lib64/libmemalloc.so:system/lib64/libmemalloc.so \
+    vendor/motorola/griffin/proprietary/vendor/lib/libHu.so:system/vendor/lib/libHu.so \
+    vendor/motorola/griffin/proprietary/vendor/lib/libHui.so:system/vendor/lib/libHui.so
 
 
 PRODUCT_COPY_FILES += \
